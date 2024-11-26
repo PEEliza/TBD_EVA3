a7d368df2f8b69a9bf0b0b4422e1c97ba648e1e7
Poleth Elizama Escarcega Lopez <124701872+PEEliza@users.noreply.github.com>
2024-11-25 18:48:24 -0700

    Add files via upload

---
+++ EVA3_9_TRIGGERS_1.sql
@@ -0,0 +1,7 @@
+delimiter $$
+create function genera_rfc2()
+returns varchar(13) deterministic
+begin
+	return "YYYY777744444";
+end $$
+delimiter ;
---
+++ EVA3_9_TRIGGERS_4.sql
@@ -0,0 +1,4 @@
+CREATE DEFINER = CURRENT_USER TRIGGER `tbd_eva3`.`pruebas_AFTER_INSERT_1` AFTER INSERT ON `pruebas` FOR EACH ROW
+BEGIN
+	set @contador = 
+END
